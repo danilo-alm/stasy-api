3d138fd9ead5a1b440b69783d23ffdc4509f3f45
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-01-28 19:11:52 -0300

    feat: add global exception handler

---
+++ src/main/java/com/stasy/api/controllers/GlobalControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.stasy.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(Exception.class)
+    String productNotFoundHandler(Exception ex) {
+        return ex.getMessage();
+    }
+}
