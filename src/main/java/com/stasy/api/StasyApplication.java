753d8998b9b6ce79754d8e981800e6030b54a5de
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-01-28 19:12:16 -0300

    feat: add main app

---
+++ src/main/java/com/stasy/api/StasyApplication.java
@@ -0,0 +1,13 @@
+package com.stasy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stasy/api/StasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stasy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
